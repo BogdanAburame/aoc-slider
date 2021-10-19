65fd9fcbcba937da5618c37925cad08c6d11c537
BogdanAburame <my_project2020@inbox.ru>
2021-10-19 17:01:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.slider__items').slick({
+  autoplay: true,
+  autoplaySpeed: 4000,
+  centerMode: true,
+  centerPadding: '1%',
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  appendArrows: $('.center'),
+})
